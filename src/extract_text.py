61c6066999ea0c006bdd405d10deba382d877259
henriquecm02 <139300033+henriquecm02@users.noreply.github.com>
2025-05-12 20:06:44 -0300

    Create extract_text.py

---
+++ src/extract_text.py
@@ -0,0 +1,8 @@
+import fitz  # PyMuPDF
+
+def extrair_texto_pdf(caminho_pdf):
+    doc = fitz.open(caminho_pdf)
+    texto = ""
+    for pagina in doc:
+        texto += pagina.get_text()
+    return texto
